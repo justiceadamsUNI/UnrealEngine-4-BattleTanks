b0d3453578d287ee622b9d384f3374f450a054aa
Justice <adamsjal@uni.edu>
2017-08-07 20:48:30 -0500

    Initial level and terrain created

---
+++ Battletanks/Source/Battletanks/Battletanks.cpp
@@ -0,0 +1,6 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "Battletanks.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, Battletanks, "Battletanks" );
---
+++ Battletanks/Source/Battletanks/BattletanksGameModeBase.h
@@ -0,0 +1,20 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "BattletanksGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class BATTLETANKS_API ABattletanksGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
